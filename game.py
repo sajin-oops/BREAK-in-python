bdb6fd918eac018c5329005a12a5c52e0e5a0ce4
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-10-10 19:20:29 +0530

    Add files via upload

---
+++ game.py
@@ -0,0 +1,17 @@
+for num in range(1, 8):  
+    if num == 5:
+        print("Found the number 5. Exiting the loop.")
+        break  
+    print(num)
+
+
+'''
+O/P
+
+1
+2
+3
+4
+Found the number 5. Exiting the loop.
+
+'''
